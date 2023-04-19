29cdda2f84e3c2f6ce5b17dde227cb6ea5a7933d
Tania <taniakalaitzidis@gmail.com>
2023-04-19 13:53:38 -0400

    Add RecipeTabView.swift as the starting point for the app

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,37 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Tania CATS on 4/19/23.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
